f20964e7cde3ebe01982e7edfda5bd5aa7328647
chacha <chachaerickjo@gmail.com>
2022-08-02 13:15:17 +0300

    test - add event listeners to switch between activities

--- app/src/main/java/com/chachaup/appealing/ui/SignInActivity.java
+++ app/src/main/java/com/chachaup/appealing/ui/SignInActivity.java
@@ -2,15 +2,37 @@ package com.chachaup.appealing.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.chachaup.appealing.R;
 
-public class SignInActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.textViewSignUp)
+    TextView mSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        ButterKnife.bind(this);
+
+        mSignUp.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mSignUp){
+            Intent intent = new Intent(SignInActivity.this, SignUpActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/chachaup/appealing/ui/SignUpActivity.java
+++ app/src/main/java/com/chachaup/appealing/ui/SignUpActivity.java
@@ -2,15 +2,35 @@ package com.chachaup.appealing.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.chachaup.appealing.R;
 
-public class SignUpActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.textViewSignIn)
+    TextView mSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        ButterKnife.bind(this);
+
+        mSignIn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mSignIn){
+            Intent intent = new Intent(SignUpActivity.this, SignInActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        }
     }
 }
